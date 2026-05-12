d76e8d922a4f47ce0215de615d0cfab1df59ede6
Gustavo Magno De Melo <gugamag2@gmail.com>
2026-05-12 20:42:14 +0000

    adiciona scripts `lint:check`, `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-
 Criando projeto do zero
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
-function Home () {
-    return <h1>Ana Vitoria, eu te amo. Feliz nosso dia.❤️</h1>
+function Home() {
+  return <h1>Ana Vitoria, eu te amo. Feliz nosso dia.❤️</h1>;
+}
+function teste() {
+  console.log("teste");
+}
+function teste2() {
+  console.log("teste dois");
 }
 export default Home;
